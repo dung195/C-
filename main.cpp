7211d2470bffc5a99441d946b8546d3383a39680
Dung <nguyentiendung100606@outlook.com.vn>
2022-10-09 15:46:41 +0700

    abc

--- main.cpp
+++ main.cpp
@@ -26,13 +26,14 @@ int getroot(int u){
     par[u]=getroot(par[u]);
     return par[u];
 }
-void connect(int u,int v){
+bool connect(int u,int v){
     u=getroot(u);
     v=getroot(v);
-    if(u==v) return;
-    if(par[u]>par[v]) swap(u,v);
+    if(u==v) return false;
+    if(par[u]<par[v]) swap(u,v);
     par[u]+=par[v];
     par[v]=u;
+    return true;
 }
 main(){
     ios_base::sync_with_stdio(0);
@@ -40,11 +41,11 @@ main(){
     cout.tie(0);
     freopen("mst.inp","r",stdin);
 	freopen("mst.out","w",stdout);
-    for(int i=0;i<sized;i++) par[i]=-1;
     int sub;
     cin>>sub;
     int m,n;
     cin>>n>>m;
+    for(int i=1;i<=n;i++) par[i]=-1;
     for(int i=1;i<=m;i++){
         int u,v,w;
         cin>>u>>v>>w;
@@ -57,9 +58,9 @@ main(){
         int v=krus[i].v;
         int w=krus[i].w;
         int pos=krus[i].pos;
-        if(getroot(u)==getroot(v)) continue;
-        connect(u,v);
-        ans.push_back(pos);
+        if (connect(u,v)) {
+           ans.push_back(pos);
+        }
     }
     for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
     cout<<endl;
