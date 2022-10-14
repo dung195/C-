673bf978de844c7dc885926b1f36ad62e9ffc4f4
Dung <nguyentiendung100606@outlook.com.vn>
2022-10-14 22:46:16 +0700

    gggg

--- main.cpp
+++ main.cpp
@@ -7,27 +7,21 @@ using namespace std;
 #define pq priority_queue
 #define bs binary_search
 #define int ll
-const ll sized = 1005;
+const ll sized = 2e6+5;
 const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-int bang[sized][sized];
+int bang[sized];
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int m,n;
-    cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++) cin>>bang[i][j];
-    }
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>bang[i];
     
-    int a;
-    cin>>a;
-    int b;
-    cin>>b;
     return 0;
 }
