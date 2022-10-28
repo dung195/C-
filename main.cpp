999467954540e32c7b5c436b7ec9b79f182fb902
Dung <nguyentiendung100606@outlook.com.vn>
2022-10-28 22:51:24 +0700

    123

--- main.cpp
+++ main.cpp
@@ -12,27 +12,16 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-int f[10]={1,1,2,6,24,120,720,5040,40320,362880};
-int fact(int x){
-    return f[x];
-}
-int cal(int x){
-    if(x>=0 && x<=9) return fact(x);
-    else return cal(x%10)+cal(x/10);
-}
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int y;
-    cin>>y;
-    for(int i=0;i<N;i++){
-        if(cal(i)==y){
-            cout<<i<<endl;
-            return 0;
-        }
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        
     }
     return 0;
 }
