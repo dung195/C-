62c08084d6abfa4fc5cc0900e0de1a244a4a439f
Dung <nguyentiendung100606@outlook.com.vn>
2022-11-02 17:46:58 +0700

    Accepted

--- main.cpp
+++ main.cpp
@@ -12,16 +12,30 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
+int bang[sized];
+int mcd[sized];
+void solve(){
+    int c,k;
+    cin>>c>>k;
+    int cnt=c/(k+1);
+    int tmp1=mcd[cnt]*2;
+    int tmp2=mcd[c]-mcd[cnt];
+    cout<<tmp1+tmp2<<endl;
+}
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        
+    int n,q;
+    cin>>n>>q;
+    for(int i=1;i<=n;i++) cin>>bang[i];
+    sort(bang+1,bang+n+1);
+    reverse(bang+1,bang+n+1);
+    for(int i=1;i<=n;i++){
+        mcd[i]=mcd[i-1]+bang[i];
     }
+    for(int i=0;i<q;i++) solve();
     return 0;
 }
