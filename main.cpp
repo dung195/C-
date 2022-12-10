c26466644154b3b6eaa97b0da198548653d44867
Dung <nguyentiendung100606@outlook.com.vn>
2022-12-10 20:46:28 +0700

    AC

--- main.cpp
+++ main.cpp
@@ -9,18 +9,49 @@ using namespace std;
 #define int ll
 const ll sized = 1e6;
 const ll N = 1e7;
-const ll inf = 1e18;
+const ll inf = 1e9;
 const ll MOD = 123456789;
 const ll LOG = 20;
 int bang[sized];
-int mcd[sized];
-void solve(){
-    int c,k;
-    cin>>c>>k;
-    int cnt=c/(k+1);
-    int tmp1=mcd[cnt]*2;
-    int tmp2=mcd[c]-mcd[cnt];
-    cout<<tmp1+tmp2<<endl;
+int st[sized*4];
+int la[sized];
+void buildtree(int l,int r,int id){
+    if(l==r){
+        st[id]=bang[l];
+        return;
+    }
+    int mid=(l+r)/2;
+    buildtree(l,mid,id*2);
+    buildtree(mid+1,r,id*2+1);
+    st[id]=max(st[id*2],st[id*2+1]);
+}
+void lazy(int id){
+    st[id]+=la[id];
+    la[id*2]+=la[id];
+    la[id*2+1]+=la[id];
+    la[id]=0;
+}
+void update(int l,int r,int u,int v,int id,int val){
+    lazy(id);
+    if(u>r || v<l) return;
+    if(l>=u && v>=r){
+        la[id]+=val;
+        lazy(id);
+        return;
+    }
+    int mid=(l+r)>>1;
+    update(l,mid,u,v,id*2,val);
+    update(mid+1,r,u,v,id*2+1,val);
+    st[id]=max(st[id*2],st[id*2+1]);
+}
+int maximum(int l,int r,int u,int v,int id){
+    lazy(id);
+    if(u>r || v<l) return -inf;
+    if(l>=u && v>=r){
+        return st[id];
+    }
+    int mid=(l+r)>>1;
+    return max(maximum(l,mid,u,v,id*2),maximum(mid+1,r,u,v,id*2+1));
 }
 main(){
     ios_base::sync_with_stdio(0);
@@ -28,14 +59,25 @@ main(){
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n,q;
-    cin>>n>>q;
+    int n;
+    cin>>n;
     for(int i=1;i<=n;i++) cin>>bang[i];
-    sort(bang+1,bang+n+1);
-    reverse(bang+1,bang+n+1);
-    for(int i=1;i<=n;i++){
-        mcd[i]=mcd[i-1]+bang[i];
+    int q;
+    cin>>q;
+    buildtree(1,n,1);
+    for(int i=0;i<q;i++){
+        int type;
+        cin>>type;
+        if(type==1){
+            int l,r,val;
+            cin>>l>>r>>val;
+            update(1,n,l,r,1,val);
+        }
+        if(type==2){
+            int l,r;
+            cin>>l>>r;
+            cout<<maximum(1,n,l,r,1)<<endl;
+        }
     }
-    for(int i=0;i<q;i++) solve();
     return 0;
 }
