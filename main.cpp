204ee79e1a85713e10263dc3ac5cf4011222ce1c
Dũng Nguyễn Tiến <nguyentiendung100606@outlook.com.vn>
2023-03-03 17:07:06 +0700

    abc

--- main.cpp
+++ main.cpp
@@ -12,43 +12,15 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-int find_deg(int x){
-    int u=log2(x);
-    return u+1;
-}
-vector<int>a;
-vector<int>b;
-map<int,bool>mp;
+
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n,x,y;
-    cin>>n>>x>>y;
-    if(x>y) swap(x,y);
-    int x_cop=x;
-    int y_cop=y;
-    while (x!=0)
-    {
-        a.push_back(x);
-        x/=2;
-    }
-    while (y!=0)
-    {
-        b.push_back(y/2);
-        mp[y]=true;
-        y/=2;
-    }
-    int pre1=0;
-    for(int i=0;i<a.size();i++){
-        int u=a[i];
-        if(mp[u]==true){
-            pre1=u;
-            break;
-        }
-    }
-    cout<<find_deg(x_cop)+find_deg(y_cop)-2*find_deg(pre1);
+    string s;
+    cin>>s;
+    
     return 0;
 }
--- main2.cpp
+++ main2.cpp
@@ -1,31 +1,122 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
-#define ll long long
-#define ld long double
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pq priority_queue
-#define bs binary_search
-#define int ll
-const ll sized = 1e6;
-const ll N = 1e7;
-const ll inf = 1e18;
-const ll MOD = 123456789;
-const ll LOG = 20;
-vector<pii>way;
-main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    // freopen("main.inp","r",stdin);
-	// freopen("main.out","w",stdout);
-    int n,m,s;
-    cin>>m>>n>>s;
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b;
-        way.push_back({a,b});
+
+class Node {
+public:
+    int l, r;
+    int max_val;
+    int count;
+    Node* left_child;
+    Node* right_child;
+
+    Node(int l, int r) {
+        this->l = l;
+        this->r = r;
+        this->max_val = 0;
+        this->count = 0;
+        this->left_child = nullptr;
+        this->right_child = nullptr;
     }
-    
-    return 0;
+
+    ~Node() {
+        delete left_child;
+        delete right_child;
+    }
+};
+
+class SegmentTree {
+public:
+    SegmentTree(vector<int>& arr) {
+        root = build_tree(arr, 0, arr.size()-1);
+    }
+
+    ~SegmentTree() {
+        delete root;
+    }
+
+    void update(int index, int value) {
+        update_node(root, index, value);
+    }
+
+    int query(int l, int r, int x) {
+        return query_node(root, l, r, x);
+    }
+
+private:
+    Node* root;
+
+    Node* build_tree(vector<int>& arr, int l, int r) {
+        if (l > r) {
+            return nullptr;
+        }
+
+        Node* node = new Node(l, r);
+
+        if (l == r) {
+            node->max_val = arr[l];
+            node->count = 1;
+            return node;
+        }
+
+        int mid = l + (r-l)/2;
+        node->left_child = build_tree(arr, l, mid);
+        node->right_child = build_tree(arr, mid+1, r);
+
+        node->max_val = max(node->left_child->max_val, node->right_child->max_val);
+        node->count = node->left_child->count + node->right_child->count;
+
+        return node;
+    }
+
+    void update_node(Node* node, int index, int value) {
+        if (node->l == index && node->r == index) {
+            node->max_val = value;
+            return;
+        }
+
+        int mid = node->l + (node->r - node->l) / 2;
+        if (index <= mid) {
+            update_node(node->left_child, index, value);
+        } else {
+            update_node(node->right_child, index, value);
+        }
+
+        node->max_val = max(node->left_child->max_val, node->right_child->max_val);
+        node->count = node->left_child->count + node->right_child->count;
+    }
+
+    int query_node(Node* node, int l, int r, int x) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        if (node->l >= l && node->r <= r) {
+            if (node->max_val < x) {
+                return 0;
+            }
+            return node->count;
+        }
+
+        int mid = node->l + (node->r - node->l) / 2;
+        int res = 0;
+
+        if (l <= mid) {
+            res += query_node(node->left_child, l, r, x);
+        }
+
+        if (r > mid) {
+            res += query_node(node->right_child, l, r, x);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    vector<int> arr = {3, 1, 5, 6, 4, 2};
+    SegmentTree tree(arr);
 }
+   
