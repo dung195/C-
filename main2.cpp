31fc90d7b29bf4fdb6a83783c46ad0b897cbee2a
dung195 <nguyentiendung100606@outlook.com.vn>
2023-05-05 14:28:09 +0700

    abc

--- main2.cpp
+++ main2.cpp
@@ -12,64 +12,41 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-vector<int>graph[sized];
-int par[sized][LOG];
-int h[sized];
-void dfs(int u){
-    for(int i=0;i<graph[u].size();i++){
-        int v=graph[u][i];
-        par[v][0]=u;
-        h[v]=h[u]+1;
-        dfs(v);
-    }
-}
-int lca(int u,int v){
-    if(h[u]<h[v]) swap(u,v);
-    for(int i=LOG-1;i>=0;i--){
-        if(h[par[u][i]]>=h[v]){
-            u=par[u][i];
-        }
-    }
-    if(u==v) return u;
-    for(int i=LOG-1;i>=0;i--){
-        if(par[u][i]!=par[v][i]){
-            u=par[u][i];
-            v=par[v][i];
-        }
-    }
-    return par[u][0];
-}
+vector<int>a;
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n,m;
-    cin>>n>>m;
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
-        graph[u].push_back(v);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        a.push_back(x);
     }
-    dfs(1);
-    for(int j=1;j<LOG;j++){
-        for(int i=1;i<=n;i++){
-            par[i][j]=par[par[i][j-1]][j-1];
+    int ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+3;j<n;j++){
+            if((j-i)%2==0) continue;
+            int n=i;
+            int m=j;
+            int tmp=inf;
+            bool ok=true;
+            while(n<m){
+                int g=a[n]+a[m];
+                if(tmp>g) tmp=g;
+                else{
+                    ok=false;
+                    break;
+                } 
+                n++;
+                m--;
+            }
+            if(ok==true) ans++;
         }
     }
-    // for(int i=1;i<=n;i++){
-    //     for(int j=0;j<=2;j++){
-    //         cout<<par[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    int q;
-    cin>>q;
-    for(int i=0;i<q;i++){
-        int u,v;
-        cin>>u>>v;
-        cout<<lca(u,v)<<" ";
-    }
+    cout<<ans%30103<<endl;
     return 0;
 }
