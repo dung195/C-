476628e4d0bfe4c5af07533959d92d6b7f267806
dung195 <nguyentiendung100606@outlook.com.vn>
2023-05-17 09:29:33 +0700

    abc

--- main.cpp
+++ main.cpp
@@ -12,32 +12,13 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-int bang[sized];
-int st[sized*4];
-void buildtree(int l,int r,int id){
-
-}
-void update(int l,int r,int u,int v,int val,int id){
-
-}
 
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
-	// freopen("main.out","w",stdout);  
-    int n,q;
-    cin>>n>>q;
-    for(int i=1;i<=n;i++)cin>>bang[i];
-    for(int i=0;i<q;i++){
-        string type;
-        cin>>type;
-        if(type=="SET"){
-            int p,c;
-            cin>>p>>c;
-            
-        }
-    }    
+	// freopen("main.out","w",stdout);
+
     return 0;
 }
--- main2.cpp
+++ main2.cpp
@@ -12,41 +12,13 @@ const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-vector<int>a;
+
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        a.push_back(x);
-    }
-    int ans=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+3;j<n;j++){
-            if((j-i)%2==0) continue;
-            int n=i;
-            int m=j;
-            int tmp=inf;
-            bool ok=true;
-            while(n<m){
-                int g=a[n]+a[m];
-                if(tmp>g) tmp=g;
-                else{
-                    ok=false;
-                    break;
-                } 
-                n++;
-                m--;
-            }
-            if(ok==true) ans++;
-        }
-    }
-    cout<<ans%30103<<endl;
+    
     return 0;
 }
