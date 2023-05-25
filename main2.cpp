b9b23906f74d36eab7ca19415b8526c4e3a5a46b
dung195 <nguyentiendung100606@outlook.com.vn>
2023-05-25 10:59:37 +0700

    abx

--- main2.cpp
+++ main2.cpp
@@ -7,18 +7,31 @@ using namespace std;
 #define pq priority_queue
 #define bs binary_search
 #define int ll
-const ll sized = 1e6;
+const ll sized = 505;
 const ll N = 1e7;
 const ll inf = 1e18;
 const ll MOD = 123456789;
 const ll LOG = 20;
-
+int bang[sized][sized];
+int mcd[sized][sized];
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    
+	int n,m;
+	cin>>m>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			cin>>bang[i][j];
+		}
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			mcd[i][j]=mcd[i][j-1]+bang[i][j];
+		}
+	}
+	
     return 0;
 }
