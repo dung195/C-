50bdd68c2024286d2cf01c4c5eb5d6281631c4d1
dung195 <nguyentiendung100606@outlook.com.vn>
2023-05-25 11:47:36 +0700

    abc

--- main.cpp
+++ main.cpp
@@ -10,40 +10,15 @@ using namespace std;
 const ll sized = 1e6;
 const ll N = 1e7;
 const ll inf = 1e18;
-const ll MOD =  1000000007;
+const ll MOD = 123456789;
 const ll LOG = 20;
-struct Node{
-    bool isend=false;
-    int child[26];
-};
-void Add(string s){
 
-}
-bool found(string s){
-
-}
-int a[sized];
-int dp[sized];
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     // freopen("main.inp","r",stdin);
 	// freopen("main.out","w",stdout);
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        string s;
-        cin>>s;
-        Add(s);
-    }
-    string g;
-    dp[0]=1;
-    for(int i=0;i<n;i++){
-        for(int j=i;j>=0;j--){
-            if(found(g.substr(j,i-j+1))==true) dp[i]=(dp[i]+dp[j])%MOD;
-        }
-    }
-    cout<<dp[g.size()-1]<<endl; 
+
     return 0;
 }
--- main2.cpp
+++ main2.cpp
@@ -1,37 +1,81 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
-#define ld long double
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pq priority_queue
-#define bs binary_search
-#define int ll
-const ll sized = 505;
-const ll N = 1e7;
-const ll inf = 1e18;
-const ll MOD = 123456789;
-const ll LOG = 20;
-int bang[sized][sized];
-int mcd[sized][sized];
-main(){
-    ios_base::sync_with_stdio(0);
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+#define pii pair<int, int>
+
+const int mod = 1e9 + 7;
+const int base = 311;
+const int LIM = 3e5 + 5;
+const int INF = 1e9;
+
+int n, m;
+int trie[LIM][27];
+int nnode = 1;
+string s;
+
+void ins(string s)
+{
+    int c = 1;
+    int len = s.length();
+    for (int i = 0; i < len; i++)
+    {
+        if (trie[c][s[i] - 'a'] == -1)
+        {
+            nnode++;
+            trie[c][s[i] - 'a'] = nnode;
+        }
+        c = trie[c][s[i] - 'a'];
+    }
+    trie[c][26] = 1;
+}
+
+int d[LIM];
+
+int dp(int k)
+{
+    if (k == s.size())
+        return 1;
+    if (d[k] != -1)
+        return d[k];
+
+    int ans = 0;
+    int c = 1;
+    for (int i = k; i < s.size(); i++)
+    {
+        if (trie[c][s[i] - 'a'] == -1)
+            break;
+        c = trie[c][s[i] - 'a'];
+        if (trie[c][26] == 1)
+            ans = (ans + dp(i + 1)) % mod;
+    }
+    return d[k] = ans;
+}
+
+int main()
+{
+    //	freopen("ten.inp", "r", stdin);
+    //	freopen("ten.out", "w", stdout);
+
+    ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    // freopen("main.inp","r",stdin);
-	// freopen("main.out","w",stdout);
-	int n,m;
-	cin>>m>>n;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			cin>>bang[i][j];
-		}
-	}
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			mcd[i][j]=mcd[i][j-1]+bang[i][j];
-		}
-	}
-	
+
+    memset(trie, -1, sizeof(trie));
+    memset(d, -1, sizeof(d));
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s;
+        ins(s);
+    }
+
+    cin >> s;
+    cout << dp(0);
+
     return 0;
 }
