ce27f91164652902c17cc718aec78d2f9db4b278
dung195 <Nguyentiendung100606@outlook.com.vn>
2023-07-29 14:09:34 +0700

    AC

--- main2.cpp
+++ main2.cpp
@@ -1,67 +1,71 @@
-#include<bits/stdc++.h>
+//Dai Ca Di Hoc
+#include <bits/stdc++.h>
+#define sz(x) int(x.size())
+#define reset(x) memset(x, 0,sizeof(x))
+#define Rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
+#define For(i,l,u) for(int (i)=(int)(l);(i)<=(int)(u);++(i))
+#define MIN(x,y) if (x > (y)) x = (y)
+#define MAX(x,y) if (x < (y)) x = (y)
+#define PB push_back
+#define mp make_pair
+#define F first
+#define S second
+#define maxn 3
+#define MOD 1000000007
+#define remain(x) if (x > MOD) x -= MOD
+#define pii pair<int, int>
+#define Task "tile"
+
 using namespace std;
-#define ll long long
-#define ld long double
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pq priority_queue
-#define bs binary_search
-#define int ll
-const ll sized = 1e6+5;
-const ll N = 1e7;
-const ll inf = 1e18;
-const ll MOD = 123456789;
-const ll LOG = 20;
-int bang[sized];
-vector<int>a;
-map<int,int> dd;
-int n,q;
-void solve(){
-	int l,r;
-	cin>>l>>r;
-	a.clear();
-	for(int i=1;i<=n;i++){
-		if(bang[i]<l || bang[i]>r) continue;
-		a.push_back(bang[i]);
-	}
-	int ans=0;
-	// cerr<<a.size()<<endl;
-	if(a.size()==0) {
-		cout<<0<<endl;
-		return;
-	}
-	for(int i=0;i<a.size()-1;i++){
-		ans+=abs(a[i]-a[i+1]);
-	}
-	cout<<ans<<endl;
-}
-void solve2(){
-	int l,r;
-	cin>>l>>r;
-	
-}
-main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    // freopen("main.inp","r",stdin);
-	// freopen("main.out","w",stdout);
-	cin>>n>>q;
-	int bien=-inf;
-	for(int i=1;i<=n;i++){
-		cin>>bang[i];
-		bien=max(bien,bang[i]);
-		dd[bang[i]]++;
-	}
-	if(n<=5000 && q<=5000){
-		for(int i=0;i<q;i++){
-			solve();
-		}
-	}
-	else{
-		for(int i=0;i<q;i++){
-			solve2();
-		}
-	}
+
+typedef long long ll;
+typedef long double ld;
+
+struct matrix{
+    int val[maxn][maxn];
+    matrix (){
+        memset(val, 0, sizeof(val));
+    }
+    matrix operator * (const matrix &B){
+        matrix C;
+        for (int i = 1; i <= 2; i++)
+            for (int j = 1; j <= 2; j++)
+            {
+                for (int k = 1; k <= 2; k++)
+                    C.val[i][j] = (C.val[i][j] + (ll)val[i][k] * B.val[k][j]) % MOD;
+            }
+        return C;
+    }
+
+    matrix POWW(ll x){
+        matrix C;
+        for (int i = 1; i <= 2; i++) C.val[i][i] = 1;
+		
+        if (x == 0) return C;
+        matrix B = POWW(x/2);
+        C = B*B;
+        if (x % 2 == 1) C = C * (*this);
+        return C;
+    }
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(); cout.tie();
+    //freopen(Task".inp", "r", stdin);
+    //freopen(Task".out", "w", stdout);
+    int ntest;
+    ll n;
+    cin >> ntest;
+    while (ntest--){
+        cin >> n;
+        matrix T;
+        T.val[1][1] = 1;
+        T.val[1][2] = 2;
+        T.val[2][1] = 1;
+        matrix B = T.POWW(n);
+        cout << B.val[1][1] << "\n";
+    }
     return 0;
 }
+
